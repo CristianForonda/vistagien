5475d4ea9998c4c5a057141f7e6aca684802f736
Cristian David Foronda <cristian.foronda123@gmail.com>
2021-06-04 23:35:12 -0500

    Re acomodo
    se reacomodo todo el contenido y se agrego login-registrar y un dashboard

---
+++ dashboard/dashboard.js
@@ -0,0 +1,53 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace()
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Lunes',
+        'Martes',
+        'Miercoles',
+        'Jueves',
+        'Viernes',
+        'Sabado',
+        'Domingo'
+      ],
+      datasets: [{
+        data: [
+          3,
+          6,
+          1,
+          0,
+          4,
+          5,
+          10
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+})()
